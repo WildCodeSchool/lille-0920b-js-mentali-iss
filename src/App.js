ccaf3e3eabcab691df1be7c5247bac094a88242f
LoicBrassart <loic.brassart@gmail.com>
2020-10-30 14:44:30 +0100

    Get back code from alien sandbox repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./reset.css";
+import "./App.css";
+import FrontPage from "./components/FrontPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page-container">
+      <Header />
+      <FrontPage />
+      <Footer />
     </div>
   );
 }
