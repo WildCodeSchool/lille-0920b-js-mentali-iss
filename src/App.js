1bcc8ff5faf66c2d0bbd7a23971fc3b28d709e9c
Guy de Messimy <gdemessimy@gmail.com>
2020-10-30 15:43:39 +0100

    remove view iss import on app.jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./reset.css";
 import "./App.css";
 import FrontPage from "./components/FrontPage";
-import ViewISS from './components/ViewISS';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
