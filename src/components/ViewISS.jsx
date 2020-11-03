b152b2815b9cf62c0d939995f4b9ce4ec2763491
Guy de Messimy <gdemessimy@gmail.com>
2020-11-03 15:39:07 +0100

    add comments

--- src/components/ViewISS.jsx
+++ src/components/ViewISS.jsx
@@ -10,6 +10,8 @@ const ISS_URL = "http://api.open-notify.org/iss-now.json";
 class ViewISS extends Component {
   constructor(props) {
     super(props);
+    //initialize a state name "SatIcon" with lat/lng (properties from leaflet)
+    //initialize a user location at false (property from leaflet)
     this.state = {
       SatIcon: {
         lat: 0,
@@ -18,19 +20,24 @@ class ViewISS extends Component {
       haveUsersLocation: false,
       zoom: 2
     }
+    //give icon function (from leaflet) to SatIcon in order to set some size ...
     this.SatIcon = L.icon({
       iconUrl: Iss,
       iconSize: [100, 100], // size of the icon
       iconAnchor: [25, 25], // point of the icon which will correspond to marker's location
     });
   }    
-
+    //mount component by call recalIss function
+    //add a callback function to update recalIss every 5s
     componentDidMount() {
       this.recalIss()
       this.interval = setInterval(this.recalIss, 2500)
     }
-
-  recalIss = () => {
+    //call API (stock in ISS-URL) by Axios
+    //import json object from API with.then
+    //update state of "issPosition" with data result from json object
+    //update haveUsersLocation ?
+    recalIss = () => {
     axios.get(ISS_URL)
     .then(({data}) => {
       console.log(data)
@@ -43,12 +50,10 @@ class ViewISS extends Component {
       zoom: 2
     })
     })
-
-  }
-
+    }
+   
   render() {
     const positionSatIcon = [this.state.SatIcon.lat, this.state.SatIcon.lng];
-
     return (
       <div> 
         <h1 id="Titre">Position de l'iSS en temps réél</h1>
@@ -63,15 +68,13 @@ class ViewISS extends Component {
       </Map>
       
       <div  className="ImgContainerButton">
-    <a href="#b" className="ChezVous" data-inf="photo">
-  
-    <img id="ImgButton" src="/photos/starObs.jpg" alt="logo" />
-    
-    <div className ="centered"> 
-    <p>Observez l'ISS </p><p>depuis chez vous </p> </div>
+        <a href="#b" className="ChezVous" data-inf="photo">
+          <img id="ImgButton" src="/photos/starObs.jpg" alt="logo" />
+      <div className ="centered"> 
+        <p>Observez l'ISS </p><p>depuis chez vous </p> </div>
         </a>   
-  </div>
-</div>
+      </div>
+      </div>
     );
   }
 }
