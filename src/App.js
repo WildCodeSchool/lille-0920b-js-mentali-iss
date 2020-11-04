0da065fd93dd36bc2b36b76d9089ff9c195fd966
Guy de Messimy <gdemessimy@gmail.com>
2020-11-04 18:25:54 +0100

    add CSS component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import "./reset.css";
 import "./App.css";
-import FrontPage from "./components/FrontPage";
-import ViewISS from './components/ViewISS';
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import CrewCards from "./components/CrewCards";
 
 function App() {
   return (
     <div className="page-container">
-      <Header />
       <CrewCards />
-      <Footer />
     </div>
   );
 }
