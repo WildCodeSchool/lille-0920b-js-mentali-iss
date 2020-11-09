9074d5693d1af6abc2eea02d668768cc708a0cd4
Guy de Messimy <gdemessimy@gmail.com>
2020-11-09 16:23:38 +0100

    traduction uk

--- src/components/InSpace2.jsx
+++ src/components/InSpace2.jsx
@@ -11,22 +11,22 @@ class InSpace2 extends React.Component {
                 name: "Sergey Ryzhikov",
                 flag: "./photos/Russia.png",
                 astroImage: "./photos/AnatoliIvanishin.jpg",
-                description: "Sergueï Nikolaïévitch Ryjikov (en russe : Сергей Николаевич Рыжиков) (aussi transcrit Ryzhikov) est un cosmonaute russe ...",
-                wiki: "https://fr.wikipedia.org/wiki/Anatoli_Ivanichine"
+                description: "Sergueï Nikolaïévitch Ryjikov (Сергей Николаевич Рыжиков) (aussi transcrit Ryzhikov) is a Russian cosmonaut ...",
+                wiki: "https://en.wikipedia.org/wiki/Sergey_Ryzhikov_(cosmonaut)"
               },
               {
                 name: "Kate Rubins",
                 flag: "./photos/Usa.png",
                 astroImage: "./photos/KateRubins.jpg",
-                description: "Kate Rubins, née le 14 octobre 1978 à Farmington au Connecticut, est une astronaute américaine ...",
-                wiki: "https://fr.wikipedia.org/wiki/Kathleen_Rubins"
+                description: "Kate Rubins is an American microbiologist and NASA astronaut....",
+                wiki: "https://en.wikipedia.org/wiki/Kathleen_Rubins"
               },
               {
                 name: "Sergey Kud-Sverchkov",
                 flag: "./photos/Russia.png",
                 astroImage: "./photos/SergeyKudSverchkov.png",
-                description: "Sergueï Vladimirovitch Koud-Skvertchkov (en russe : Сергей Владимирович Кудь-Сверчков) est un cosmonaute russe ...",
-                wiki: "https://fr.wikipedia.org/wiki/Sergue%C3%AF_Koud-Skvertchkov"
+                description: "Sergueï Vladimirovitch Koud-Skvertchkov (Сергей Владимирович Кудь-Сверчков) was born on August 23, 1983 at the  ...",
+                wiki: "https://en.wikipedia.org/wiki/Sergey_Kud-Sverchkov"
               },
             ],
         };
