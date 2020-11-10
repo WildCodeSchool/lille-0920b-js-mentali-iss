f601ff646af930ef3b4fb78e002808e3e95d0480
Guy de Messimy <gdemessimy@gmail.com>
2020-11-10 12:01:59 +0100

    fix player call

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import "./reset.css";
 import "./App.css";
 import InSpace2 from "./components/InSpace2";
-import Video from "./components/Testlecteurs"
 
 function App() {
   return (
     <div className="page-container">
-      <Video />
+      <InSpace2 />
     </div>
   );
 }
