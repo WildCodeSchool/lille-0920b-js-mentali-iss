508d5360a1f9047238f00f05c24b8a72bf65d2ab
Guy de Messimy <gdemessimy@gmail.com>
2020-11-10 11:59:56 +0100

    testing player before create branch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./reset.css";
 import "./App.css";
 import InSpace2 from "./components/InSpace2";
+import Video from "./components/Testlecteurs"
 
 function App() {
   return (
     <div className="page-container">
-      <InSpace2 />
+      <Video />
     </div>
   );
 }
