c350e28bb6b6d321687da361aa5bcbd3e08c7f51
Guy de Messimy <gdemessimy@gmail.com>
2020-11-12 14:51:35 +0100

    testing

--- src/components/InSpace2.jsx
+++ src/components/InSpace2.jsx
@@ -8,21 +8,21 @@ class InSpace2 extends React.Component {
       this.state={
         astrolist:[
             {
-                nameAs: "Sergey Ryzhikov",
+                name: "Sergey Ryzhikov",
                 flag: "./photos/Russia.png",
                 astroImage: "./photos/AnatoliIvanishin.jpg",
                 description: "Sergueï Nikolaïévitch Ryjikov (Сергей Николаевич Рыжиков) (aussi transcrit Ryzhikov) is a Russian cosmonaut ...",
                 wiki: "https://en.wikipedia.org/wiki/Sergey_Ryzhikov_(cosmonaut)"
               },
               {
-                nameAs: "Kate Rubins",
+                name: "Kate Rubins",
                 flag: "./photos/Usa.png",
                 astroImage: "./photos/KateRubins.jpg",
                 description: "Kate Rubins is an American microbiologist and NASA astronaut....",
                 wiki: "https://en.wikipedia.org/wiki/Kathleen_Rubins"
               },
               {
-                nameAs: "Sergey Kud-Sverchkov",
+                name: "Sergey Kud-Sverchkov",
                 flag: "./photos/Russia.png",
                 astroImage: "./photos/SergeyKudSverchkov.png",
                 description: "Sergueï Vladimirovitch Koud-Skvertchkov (Сергей Владимирович Кудь-Сверчков) was born on August 23, 1983 at the  ...",
@@ -36,8 +36,8 @@ render () {
   return (
     <div>
       <div>
-         {this.state.astrolist.map((nameAs, index) => (
-        <MapPageCards2 key={index} {...nameAs} />
+         {this.state.astrolist.map((name, index) => (
+        <MapPageCards2 key={index} {...name} />
       ))}
       </div>
     </div>
--- src/components/MapPageCards.jsx
+++ src/components/MapPageCards.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect}  from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 const Crewcards = styled.div 
@@ -83,36 +83,14 @@ const Learnmore = styled.a
     color: blue;
     }`
 
-function CrewCards ({nameAs, craft}) {
-
-const [people, setPeople] = useState("");
-console.log("component re-render")
-
-useEffect(() => {
-    getCrew(){
-        axios
-          .get('http://api.open-notify.org/astros.json')
-          .then((response) => response.data)
-          .then((data) => {
-            console.log(data.people)
-            setPeople({
-            people: data.people,
-            });
-          });
-        };
-    }
-);
-
-return (
-
-<Crewcards>
+const CrewCards = ({name, craft}) => (
+      <Crewcards>
         <Crewheader>
           <Astrophoto src="./photos/IvanWagner.jpg"  alt="Ivan Wagner" />
         </Crewheader>
         <Crewsection>
           <Astrotitle>
-            <Astroname>{people.map((name, index, craft) => (
-            {name}))}</Astroname>
+            <Astroname>{name} in {craft}</Astroname>
             <Astroflag src="./photos/Russia.png" alt="drapeau" />
           </Astrotitle>
           <Trait></Trait>
@@ -123,7 +101,6 @@ return (
              <Learnmore href="https://fr.wikipedia.org/wiki/Ivan_Vagner">Learn more ...</Learnmore>
         </Crewsection> 
       </Crewcards>
-)
-}
+    );
 
 export default CrewCards;
