7912021c5a8d3291087eca87c512e68cdae96805
Marie Garde <marie_garde@ymail.com>
2020-11-15 11:26:07 +0100

    Boucles for fonctionnelles

--- src/components/ChooseADate.jsx
+++ src/components/ChooseADate.jsx
@@ -65,6 +65,34 @@ const Submit = styled.input`
 `;
 
 class ChooseADate extends Component {
+  buildDays() {
+    var arrDay = [];
+
+    for (let i = 1; i <= 31; i++) {
+      arrDay.push(
+        <option key={i} value={i}>
+          {i}
+        </option>
+      );
+    }
+
+    return arrDay;
+  }
+
+  buildYear() {
+    var arrYear = [];
+
+    for (let i = 1996; i <= 2020; i++) {
+      arrYear.push(
+        <option key={i} value={i}>
+          {i}
+        </option>
+      );
+    }
+
+    return arrYear;
+  }
+
   constructor(props) {
     super(props);
     this.state = {
@@ -131,37 +159,7 @@ class ChooseADate extends Component {
               <label>
                 <Select value={this.state.day} onChange={this.handleChangeDay}>
                   <option value="day">Day</option>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                  <option value="5">5</option>
-                  <option value="6">6</option>
-                  <option value="7">7</option>
-                  <option value="8">8</option>
-                  <option value="9">9</option>
-                  <option value="10">10</option>
-                  <option value="11">11</option>
-                  <option value="12">12</option>
-                  <option value="13">13</option>
-                  <option value="14">14</option>
-                  <option value="15">15</option>
-                  <option value="16">16</option>
-                  <option value="17">17</option>
-                  <option value="18">18</option>
-                  <option value="19">19</option>
-                  <option value="20">20</option>
-                  <option value="21">21</option>
-                  <option value="22">22</option>
-                  <option value="23">23</option>
-                  <option value="24">24</option>
-                  <option value="25">25</option>
-                  <option value="26">26</option>
-                  <option value="27">27</option>
-                  <option value="28">28</option>
-                  <option value="29">29</option>
-                  <option value="30">30</option>
-                  <option value="31">31</option>
+                  {this.buildDays()}
                 </Select>
               </label>
               <label>
@@ -189,32 +187,8 @@ class ChooseADate extends Component {
                   value={this.state.year}
                   onChange={this.handleChangeYear}
                 >
-                  <option value="year">Year</option>
-                  <option value="2020">2020</option>
-                  <option value="2019">2019</option>
-                  <option value="2018">2018</option>
-                  <option value="2017">2017</option>
-                  <option value="2016">2016</option>
-                  <option value="2015">2015</option>
-                  <option value="2014">2014</option>
-                  <option value="2013">2013</option>
-                  <option value="2012">2012</option>
-                  <option value="2011">2011</option>
-                  <option value="2010">2010</option>
-                  <option value="2009">2009</option>
-                  <option value="2008">2008</option>
-                  <option value="2007">2007</option>
-                  <option value="2006">2006</option>
-                  <option value="2005">2005</option>
-                  <option value="2004">2004</option>
-                  <option value="2003">2003</option>
-                  <option value="2002">2002</option>
-                  <option value="2001">2001</option>
-                  <option value="2000">2000</option>
-                  <option value="1999">1999</option>
-                  <option value="1998">1998</option>
-                  <option value="1997">1997</option>
-                  <option value="1996">1996</option>
+                  <option value="yaer">Year</option>
+                  {this.buildYear()}
                 </Select>
               </label>
               <Submit type="submit" value="Submit" />
@@ -224,7 +198,7 @@ class ChooseADate extends Component {
             {this.state.photo ? (
               <DisplayChooseADate photo={this.state.photo} />
             ) : (
-              <ChooseADateImg src="/photos/choseadate.png" alt="chose a date" />
+              <ChooseADateImg src="/photos/choseadate.png" alt="cook" />
             )}
           </div>
         </Textcolor>
