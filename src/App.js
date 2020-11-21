b8ed4525c3e47fec017496ef6e78cf6594e752f9
Guy de Messimy <gdemessimy@gmail.com>
2020-11-21 18:15:31 +0100

    delete Galerie Page import

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import MyCarousel from "./components/CarouselMap";
 import ViewISS from "./components/ViewISS";
 import AstroList from "./components/AstroList";
 import Footer from "./components/Footer";
-import GaleriePage from "./components/GaleriePage";
 
 const size = {
   xs: '320px',
